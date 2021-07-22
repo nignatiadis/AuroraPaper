f25f28a5427285341e7efc2b41a5942fad64cf4b
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2021-07-22 13:06:42 +0300

    some more tests

--- AuroraR/tests/testthat/test-simulations.R
+++ AuroraR/tests/testthat/test-simulations.R
@@ -41,3 +41,49 @@ expect_true(abs(mse_midrange - 6*sigma_2^2*B_2/( (B_2+1)*(B_2+2))) < 0.001)
 
 
 
+
+
+# Test Pareto
+
+set.seed(1)
+tmp_alpha <- 6
+tmp_B <- 5000
+tmp_m <- 10000
+pareto_sim <- uniform_pareto_sim(m=tmp_m, B=tmp_B, pareto_alpha=tmp_alpha, unif_lb=0.3, unif_ub=5)
+
+expect_true(range(pareto_sim$true_mus)[1] >= 0.3)
+expect_true(range(pareto_sim$true_mus)[2] <= 5)
+
+pareto_mles <- pareto_mle(pareto_sim$Zs)
+pareto_means <- location_mean(pareto_sim$Zs)
+
+mean(pareto_mles - pareto_sim$true_mus)
+mean(pareto_means - pareto_sim$true_mus)
+
+pareto_mle_error <- mean( (pareto_mles - pareto_sim$true_mus)^2)
+
+pareto_mean_error <- mean( (pareto_means - pareto_sim$true_mus)^2)
+expect_true(pareto_mean_error > pareto_mle_error)
+expect_true( (pareto_mean_error - mean(pareto_sim$sigmas^2 / 100 ))/pareto_mean_error <= 0.01)
+
+true_xs <- pareto_sim$true_mus*(tmp_alpha - 1)/tmp_alpha
+true_medians <- true_xs * 2^(1/tmp_alpha)
+
+true_pdfs <- tmp_alpha*true_xs^(tmp_alpha)/( true_medians^(tmp_alpha+1))
+medians_vars <- 1/(tmp_B*4*true_pdfs^2)
+  
+  
+pareto_medians <- location_median(pareto_sim$Zs)
+expect_true(length(pareto_medians) == tmp_m)
+
+expect_true( abs(mean(pareto_medians - true_medians)) < sqrt(mean(medians_vars)/tmp_m)*4) 
+abs(mean(pareto_medians - pareto_sim$true_mus))
+
+pareto_median_error <- mean( (pareto_medians - pareto_sim$true_mus)^2)
+
+pareto_median_median_error <- mean( (pareto_medians -true_medians)^2)
+expect_true( pareto_median_median_error <= 1.1*mean(medians_vars))
+expect_true( pareto_median_median_error >= 0.9*mean(medians_vars))
+
+expect_true(pareto_median_error > pareto_median_median_error)
+expect_true(pareto_mean_error < pareto_median_error)
